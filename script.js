1a19de6ecef9e97571858a892e9fcbbe87acd1f2
shaban <146794146+sarzhh@users.noreply.github.com>
2024-10-18 15:26:29 +0000

    Add blog buttons and view button logic

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const blog = document.querySelector(".blog");
 const newPostButton = document.querySelector("#newPostButton");
 const overlay = document.querySelector(".overlay");
 const newPostModal = document.querySelector(".new-post-modal");
@@ -6,6 +7,10 @@ const submitButton = document.querySelector(".submitButton");
 const blogContainer = document.querySelector(".blog-container");
 const newPostTitle = document.querySelector("#new-post-title");
 const newPostContent = document.querySelector("#new-post-content");
+const viewPage = document.querySelector(".view-page");
+const viewTitle = document.querySelector(".view-title");
+const viewContent = document.querySelector(".view-content");
+const viewBackButton = document.querySelector(".view-back-button")
 
 let currentBlogData = null;
 
@@ -22,21 +27,50 @@ function closeModal() {
 newPostButton.addEventListener("click", openModal);
 newPostCloseButton.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
+viewBackButton.addEventListener('click', ()=> {
+    blog.style.display = "block"
+    viewPage.style.display = "none"
+    });
+
 
 function createBlogTile(blogData) {
     const blogTile = document.createElement('div');
+    const blogWriting = document.createElement('div');
     const blogTitle = document.createElement('h3');
     const blogContent = document.createElement('p');
+    const blogButtons = document.createElement('div');
+    const viewButton = document.createElement('button');
+    const editButton = document.createElement('button');
+    const deleteButton = document.createElement('button')
 
     blogTile.classList.add('blog-tile');
     blogTitle.classList.add('blog-title');
     blogContent.classList.add('blog-content');
+    blogButtons.classList.add('blog-buttons-container');
+    viewButton.classList.add('blog-buttons');
+    editButton.classList.add('blog-buttons');
+    deleteButton.classList.add('blog-buttons');
 
     blogTitle.textContent = blogData.title;
-    blogContent.textContent = blogData.content.slice(0, 30) + "...";
+    blogContent.textContent = blogData.content.slice(0, 50) + "...";
+    viewButton.textContent =  "view";
+    editButton.textContent =  "edit";
+    deleteButton.textContent =  "delete";
 
-    blogTile.append(blogTitle, blogContent);
+    blogWriting.append(blogTitle, blogContent)
+    blogButtons.append(viewButton, editButton, deleteButton)
+    blogTile.append(blogWriting, blogButtons);
     blogContainer.appendChild(blogTile);
+
+    viewButton.addEventListener('click',() => viewBlog(blogData));
+};
+
+function viewBlog(blogData) {
+    viewTitle.textContent = blogData.title;
+    viewContent.textContent = blogData.content;
+
+    blog.style.display = "none";
+    viewPage.style.display ="block";
 };
 
 function storeData(blogData) {
